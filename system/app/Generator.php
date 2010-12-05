87e6a0bc0eba0b628f73182873d19ea28f9c1fea
kaliwanagan <kaliwanagan@openkore.com>
2010-12-05 10:24:31 +0800

    add licnese header to files

--- system/app/Generator.php
+++ system/app/Generator.php
@@ -1,4 +1,23 @@
 <?php
+/***
+This file is part of Tensai.
+
+Tensai is free software: you can redistribute it and/or modify
+it under the terms of the Affero GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Tensai is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+Affero GNU General Public License for more details.
+
+You should have received a copy of the Affero GNU General Public License
+along with Tensai.  If not, see <http://www.gnu.org/licenses/>.
+***/
+?>
+
+<?php
 /**
 *  Page generation engine
 */
